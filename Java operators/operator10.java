89488b79917a314cc6dd228a98826b6c34e5d672
Kalai0001 <kalai1172000@gmail.com>
2026-02-17 21:14:53 +0530

    Add files via upload

---
+++ Java operators/operator10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class operator10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int unit=sc.nextInt();
+        int voltage=sc.nextInt();
+        if(unit>500 || voltage==1){
+            System.out.println("Alert");
+        }
+        else{
+            System.out.println("Normal");
+        }
+        sc.close();
+    }
+}
