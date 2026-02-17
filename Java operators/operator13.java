0b3d08d90378c38d18dc00cf35d16c118ba5f0c4
Kalai0001 <kalai1172000@gmail.com>
2026-02-17 21:16:03 +0530

    Add files via upload

---
+++ Java operators/operator13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class operator13 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int connectionvalue=sc.nextInt();
+        int appliancemask=sc.nextInt();
+        int result=connectionvalue & appliancemask ;
+        if(result!=0){
+            System.out.println("Active");
+        }
+        else{
+            System.out.println("Inactive");
+        }
+        sc.close();
+    }
+}
