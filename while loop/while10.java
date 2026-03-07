cae6ab35aaebed05c599314f69c5037ed9164ec7
Kalai0001 <kalai1172000@gmail.com>
2026-03-07 23:21:06 +0530

    Add files via upload

---
+++ while loop/while10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class while10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=n;
+        int a=0;
+        int b=1;
+        System.out.printf("Fibonacci sequence(%d terms)\n",n);
+        while(x!=0){
+            // System.out.printf("Fibonacci sequence(%d terms)\n",n);
+            System.out.printf("%d ",a);
+            int c=a+b;
+            a=b;
+            b=c;
+            x--;
+        }
+        sc.close();
+    }
+}
